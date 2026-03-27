70e608c9d5f197e6999a0bf85a64587c0a2ed0a2
uju <uju031104@gmail.com>
2026-03-27 17:04:42 +0900

    Monster.h/Monster.cpp 추가

---
+++ Text_ConsoleRPG/Monster.cpp
@@ -0,0 +1,119 @@
+#include "Monster.h"
+
+// Monster 몬스터 
+Monster::Monster(std::string name) : Character(name)
+{
+	this->name = name;
+}
+
+void Monster::attack(Player* player)
+{
+	std::cout << this->name << "이(가)" << player << "을(를) 공격했다!" << std::endl;
+	// player->setCurrentHP() = player->getCurrentHP() - this->getATK();
+}
+
+void Monster::death()
+{
+	std::cout << this->name << "이(가) 쓰러졌다!" << std::endl;
+}
+
+void Monster::printStatus()
+{
+	std::cout << "몬스터의 이름: " << name << std::endl;
+	std::cout << "몬스터의 레벨: " << level << std::endl;
+	std::cout << "몬스터의 최대 HP: " << maxHP << std::endl;
+	std::cout << "몬스터의 현재 HP: " << currentHP << std::endl;
+	std::cout << "몬스터의 공격력: " << ATK << std::endl;
+	std::cout << "몬스터의 속도: " << speed << std::endl;
+}
+
+std::string Monster::getItems() const
+{
+	return this->items;
+}
+
+int Monster::getMoney() const
+{
+
+	return this->money;
+}
+
+// Monster를 상속받은 실제로 생성할 몬스터 클래스들 구현
+// 멤버 변수들 값은 일단 임의로 설정한 상태
+// Chonkycat 뚱냥이
+Chonkycat::Chonkycat() : Monster("뚱냥이")
+{
+	this->maxHP = 100;
+	this->currentHP = 100;
+	this->ATK = 10;
+	this->speed = 10;
+	this->level = 1;
+	this->exp = 10;
+	this->items = "뚱냥이의 털뭉치";
+	this->money = 100;
+}
+
+
+
+// Bulldog 불독
+Bulldog::Bulldog() : Monster("불독")
+{
+	this->maxHP = 200;
+	this->currentHP = 200;
+	this->ATK = 20;
+	this->speed = 15;
+	this->level = 2;
+	this->exp = 30;
+	this->items = "불독의 턱살";
+	this->money = 200;
+}
+
+
+
+// Ghost 고스트
+Ghost::Ghost() : Monster("고스트")
+{
+	this->maxHP = 100;
+	this->currentHP = 100;
+	this->ATK = 50;
+	this->speed = 50;
+	this->level = 3;
+	this->exp = 50;
+	this->items = "고스트의 영혼";
+	this->money = 500;
+}
+
+
+
+// Golem 골렘
+Golem::Golem() : Monster("골렘")
+{
+	this->maxHP = 500;
+	this->currentHP = 500;
+	this->ATK = 100;
+	this->speed = 1;
+	this->level = 4;
+	this->exp = 100;
+	this->items = "골렘의 돌조각";
+	this->money = 1000;
+}
+
+
+
+// LuckyMonster 럭키 몬스터
+LuckyMonster::LuckyMonster() : Monster("???")
+{
+	// 난수 생성
+	std::random_device rd;
+	std::mt19937 generate(rd());
+
+
+	this->maxHP = std::uniform_int_distribution<int>(50, 500)(generate);
+	this->currentHP = this->maxHP;
+	this->ATK = std::uniform_int_distribution<int>(5, 100)(generate);
+	this->speed = std::uniform_int_distribution<int>(1, 100)(generate);
+	this->level = std::uniform_int_distribution<int>(1, 4)(generate);
+	this->exp = std::uniform_int_distribution<int>(1, 200)(generate);
+	this->items = "행운의 증표";
+	this->money = std::uniform_int_distribution<int>(1, 5000)(generate);
+}
---
+++ Text_ConsoleRPG/Monster.h
@@ -0,0 +1,74 @@
+#pragma once
+#pragma once
+#include "Character.h"
+#include <string>
+#include <random>
+#include <iostream>
+class Enemy;
+class Player;
+
+class Monster : public Character
+{
+public:
+	Monster(std::string name);
+	void attack(Enemy* enemy) override {}; // 지금 못씀
+	void attack(Player* player);           // 나중에 이런식으로
+	void death() override final;
+	void printStatus() override final;
+
+	virtual ~Monster() {} // 업캐스팅한 객체 소멸할때 필요
+
+	// getter
+	// 몬스터는 따로 인벤/돈 관리를 하지않고 단순하게 아이템/돈을 가지고 있고, 죽으면 드랍
+	std::string getItems() const;
+	int getMoney() const;
+
+protected:
+	std::string items;
+	int money;
+};
+
+
+// Monster를 상속받은 실제로 생성할 몬스터 클래스들 정의
+//
+// 첫번째 몬스터: 뚱냥이
+class Chonkycat : public Monster
+{
+public:
+	Chonkycat();
+	~Chonkycat() {}
+};
+
+// 두번째 몬스터: 불독
+class Bulldog : public Monster
+{
+public:
+	Bulldog();
+	~Bulldog() {}
+};
+
+// 세번째 몬스터: 고스트
+class Ghost : public Monster
+{
+public:
+	Ghost();
+	~Ghost() {}
+};
+
+// 네번째 몬스터: 골렘
+class Golem : public Monster
+{
+public:
+	Golem();
+	~Golem() {}
+};
+
+// 럭키 몬스터(랜덤 몬스터)
+// HP, 공격력 등 스탯 랜덤(뚱냥이보다 약할수도 골렘보다 셀 수도 있음)
+// 죽었을때 플레이어에게 주는 경험치, 돈도 랜덤
+class LuckyMonster : public Monster
+{
+public:
+	LuckyMonster();
+	~LuckyMonster() {}
+};
