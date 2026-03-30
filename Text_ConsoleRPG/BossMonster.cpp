33851d7b5d39747e829254032a95ba3ed9e2f478
Seju KIM <lutablis@gmail.com>
2026-03-30 10:20:04 +0900

    BossMonster 전투 함수 구현 완료

--- Text_ConsoleRPG/BossMonster.cpp
+++ Text_ConsoleRPG/BossMonster.cpp
@@ -3,7 +3,7 @@
 
 #include "BossMonster.h"
 
-BossMonster::BossMonster() :Character("초대형 울트라 찍찍 들쥐")
+BossMonster::BossMonster() :Character("[초대형 울트라 찍찍 들쥐]")
 {
 	// 모든 수치는 공유 후 조율할 예정
 	maxHP = 10000;
@@ -24,7 +24,8 @@ int BossMonster::determinePhase(int currentHP)
 	{
 		phase = 2;
 	}
-	else {
+	else
+	{
 		phase = 3;
 	}
 
@@ -34,6 +35,8 @@ int BossMonster::determinePhase(int currentHP)
 void BossMonster::attack(Character* enemy)
 {	
 	int pattern = 0;
+
+	isInvincible = false;
 
 	switch (determinePhase(currentHP))
 	{
@@ -51,36 +54,128 @@ void BossMonster::attack(Character* enemy)
 	switch (pattern)
 	{
 	case 1:
-		std::cout << name << "이(가) [꼬리 휘두르기]를 시전했습니다!\n\n";
-
-		// enemy->setCurrentHP(enemy->getCurrentHP() - (ATK));
+		std::cout << name << "가 [꼬리 휘두르기]를 시전했습니다!\n\n";
+		enemy->setCurrentHP(enemy->getCurrentHP() - (ATK));
+		enemy->takeDamage(ATK);
 		break;
 	case 2:
-		std::cout << name << "이(가) [산성 침 뱉기]를 시전했습니다!\n\n";
-
-		// enemy->setCurrentHP(enemy->getCurrentHP() - (ATK * 0.7));
+		std::cout << name << "가 [산성 침 뱉기]를 시전했습니다!\n\n";
+		enemy->setCurrentHP(enemy->getCurrentHP() - (ATK * 0.7));
+		enemy->takeDamage(ATK * 0.7);
 		break;
 	case 3:
-		std::cout << name << "이(가) [고양이 잡는 쥐]를 시전했습니다.\n";
+		std::cout << name << "가 [고양이 잡는 쥐]를 시전했습니다.\n";
 		std::cout << "손톱이 날아옵니다!\n\n";
-
-		// enemy->setCurrentHP(enemy->getCurrentHP() - (ATK * 1.7));
-		break;
-	case 4:
-		attackPattern4(enemy);
+		enemy->setCurrentHP(enemy->getCurrentHP() - (ATK * 1.7));
+		enemy->takeDamage(ATK * 1.7);
 		break;
 	case 5:
-		heal();
+		std::cout << name << "가 메가 [썩은 치즈]를 먹었습니다!\n";
+		setCurrentHP(getCurrentHP() + 100);
+		std::cout << name << "의 체력이 100 회복되었습니다.\n\n";
 		break;
 	case 6:
 		isInvincible = true;
+		std::cout << name << "가 ??????다!\n";
+		std::cout << name << "를 향한 다음 공격이 1회 무효화됩니다.\n\n";
 		// Player쪽에서 만약 isInvincible = true;면 대미지 0을 넣는 거로
 		break;
 	case 7:
-		// 궁극기
+		std::cout << name << "가 ~~~~를 시전했습니다!";
+		ATK += 50; // 수치 추후 조율
+		std::cout << name << "의 공격력이 추가로 50 올랐습니다!\n\n";
 		break;
 	default:
 		std::cout << "공격 패턴 출력 과정에서 오류 발생\n";
+		break;
+	case 4: //얘를 맨 밑에 두지 않으면 빌드 실패함..
+		// static cast인지 dynamic cast인지, 그리고 아래 체크 필요한지 확인
+		Player * player = dynamic_cast<Player*>(enemy);
+		if (player != nullptr)
+		{
+			attackPattern4(player);
+		}
+		else
+		{
+			std::cout << "오류: enemy가 Player 타입이 아닙니다.\n";
+		}
+		attackPattern4(player);
+		break;
+	}
+}
+
+void BossMonster::attackPattern4(Player* player)
+{
+	std::cout << name << "가 퀴즈 공격을 시전합니다!\n\n";
+	std::cout << name << ": 너처럼 나쁜 아이는 벌을 받아야 한다 찍찍...\n";
+	std::cout << name << ": 하지만 그 전에 너를 평가하겠다 찍찍!\n";
+	std::cout << name << ": 과연 내가 내는 퀴즈를 맞출 수 있을까 찍찍~?!?!?!\n\n";
+
+	int a = rand() % 15 + 1;
+	int b = rand() % 15 + 1;
+
+	int random = rand() % 4 + 1;
+
+	std::cout << "다음 퀴즈의 정답은 뭘까?";
+
+	switch (random)
+	{
+	case 1:
+		// quiz #1
+		std::cout << "int a는 " << a << "이고, int b는 " << b << "이다.\n";
+		std::cout << "a % b의 값은 ?\n\n";
+		correctAnswer = a % b;
+		break;
+	case 2:
+		// quiz #2
+		std::cout << "int a는 " << a << "이고, int b는 " << b << ",\n";
+		std::cout << "int answer = (a < b) ? a : b일 때\n";
+		std::cout << "answer의 값은?\n\n";
+		correctAnswer = (a < b) ? a : b;
+		break;
+	case 3:
+		// quiz #3
+		correctAnswer = (a < 7 && b > 7);
+		std::cout << "int a는 " << a << "이고, int b는 " << b << ",\n";
+		std::cout << "int answer = (a < 7 && b > 7)일 때\n";
+		std::cout << "answer의 값은?\n\n";
+		break;
+	case 4:
+		// quiz #4
+		std::cout << "int a는 " << a << "이고, int b는 " << b << ",\n";
+		std::cout << "answer = (a & b)의 값은?\n"; // 이거 배웠나?
+		std::cout << "answer의 값은?\n\n";
+		correctAnswer = (a & b);
+		break;
+	}
+}
+
+void BossMonster::takeDamage(int amount)
+{
+	currentHP -= amount;
+
+	std::cout << name << "에게 " << amount << "의 피해를 입혔습니다!\n";
+
+	if (currentHP <= 0) {
+		currentHP = 0;
+		death();
+	}
+}
+
+void BossMonster::attackPattern4CheckAnswer(Player* player)
+{
+	if (true/*true 지우고 이거 player->getPlayerAnswer() != correctAnswer*/)
+	{
+		std::cout << name << ": 푸하하 틀렸지롱~\n";
+		std::cout << name << "가 공부하지 않은 님을 응징하고 체력을 회복합니다!\n";
+		player->setCurrentHP(player->getCurrentHP() - (ATK));
+		currentHP += ATK;
+	}
+	else
+	{
+		std::cout << name << ": 정답을 맞추다니...!! 이거 실화냐 찍찍~~??\n";
+		std::cout << player->getName() << "은 혼란을 틈타 " << name << "를 공격했습니다.\n\n";
+		takeDamage(player->getATK());
 	}
 }
 
@@ -94,46 +189,25 @@ void BossMonster::printStatus()
 void BossMonster::death()
 {
 	std::cout << name << "크윽... 내가 지다니...!! 찍...\n";
-	std::cout << name << "을(를) 쓰러뜨렸습니다~!!\n\n";
+	std::cout << name << "를 쓰러뜨렸습니다~!!\n\n";
+	
+	// 전투하면서 올랐던 공격력 수치 초기화.
+	// 만약 보스 죽이고 게임이 아예 끝난다면 이거 필요 없음.
+	ATK = 100;
 }
 
-void BossMonster::attackPattern4(Character* enemy)
-{
-	std::cout << name << "이(가) 퀴즈 공격을 시전합니다!\n\n";
-	std::cout << name << ": 너처럼 나쁜 아이는 벌을 받아야 한다 찍찍...\n";
-	std::cout << name << ": 하지만 그 전에 너를 평가하겠다 찍찍!\n";
-	std::cout << name << ": 과연 내가 내는 퀴즈를 맞출 수 있을까 찍찍~?!?!?!\n\n";
-
-	int random = rand() % 4 + 1;
-
-	switch (random)
-	{
-	case 1:
-		// quiz #1
-		break;
-	case 2:
-		// quiz #2
-		break;
-	case 3:
-		// quiz #3
-		break;
-	case 4:
-		//quiz #4
-		break;
-	}
-}
-
-void BossMonster::heal()
-{
-	setCurrentHP(getCurrentHP()+100);
-}
-
+// getters
 bool BossMonster::getIsInvincible()
 {
 	return isInvincible;
 }
-
-void BossMonster::setIsInvincible(bool isInvincible)
+int BossMonster::getPhase()
 {
-	this->isInvincible = isInvincible;
+	return phase;
 }
+int BossMonster::getCorrectAnswer()
+{
+	return correctAnswer;
+}
+
+// setters
--- Text_ConsoleRPG/BossMonster.h
+++ Text_ConsoleRPG/BossMonster.h
@@ -2,31 +2,39 @@
 #include <string>
 
 #include "Character.h"
+#include "Player.h"
+
+//전방선언 필요?
 
 class BossMonster :public Character
 {
 public:
 	BossMonster();
+	// 소멸자 해야하나?
 	
 	void attack(Character* enemy) override;
+
+	void takeDamage(int amount) override;
 
 	void printStatus() override;
 	
 	void death() override;
 
-	void attackPattern4(Character* enemy);
+	void attackPattern4(Player* player);
+
+	void attackPattern4CheckAnswer(Player* player);
 
 	int determinePhase(int currentHP);
 
-	void heal();
-
 	// getters
+	int getCorrectAnswer();
+	int getPhase();
 	bool getIsInvincible();
 
 	// setters
-	void setIsInvincible(bool isInvincible);
 
 private:
 	bool isInvincible = false;
 	int phase = 1;
+	int correctAnswer = 0;
 };
